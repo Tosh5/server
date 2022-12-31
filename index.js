9f9dfc7e67fcc7c2ead068fab763b9090c4726be
Tosh5 <toshiya.5601048.com@gmail.com>
2022-12-31 15:26:45 +0900

    12311524

--- index.js
+++ index.js
@@ -36,8 +36,60 @@ const index_bin = [...Array(index_bin_size)].map((_, i) => i);
 	}
 
 const indexIo = io.of("/index")
-indexIo.on("connection", (socket) =>{
-    console.log('connected to namespace /index')
+// indexIo.on("connection", (socket) =>{
+//     console.log('connected to namespace /index')
+//     socket.on("send_myindex", async (data)=>{
+
+//         console.log(data)
+
+//         socket.emit("receive_message2", data);
+
+//         index_bin.splice(0,1);
+//         index_bin.push(data)
+
+//         let sum = 0;
+
+//         // 総和を取得
+//         for (let i = 0; i < index_bin.length; i++) {
+//             sum += index_bin[i];
+//         }
+
+//         let aveIndex = String(sum / index_bin.length)
+
+//         console.log(aveIndex)
+
+//         try {
+//             socket.emit("receive_message2", aveIndex);
+//             // socket.emit("receive_message2", {value : aveIndex});
+//             console.log(`ave_index is ${aveIndex}`)
+//           } catch (error) {
+//             console.error(error);
+//             // expected output: ReferenceError: nonExistentFunction is not defined
+//             // (Note: the exact output may be browser-dependent)
+//           }
+          
+
+//         // await socket.emit("ave_index", aveIndex)
+//         // await socket.emit("ave_index", {aveIndex})
+//         // socket.emit("ave_index", {aveIndex})
+
+//         // console.log(`ave_index is ${aveIndex}`)
+        
+        
+//     })
+
+// }) 
+io.on("connection", (socket) =>{
+    console.log(`User Connected: ${socket.id}`)
+
+    socket.on("send_message", (data)=>{
+        console.log(data)
+        // socket.to(data).emit("received_message", data)
+        // socket.broadcast.emit("receive_message", data)
+        socket.emit("receive_message", data)
+    
+    })
+
     socket.on("send_myindex", async (data)=>{
 
         console.log(data)
@@ -77,58 +129,6 @@ indexIo.on("connection", (socket) =>{
         
         
     })
-
-}) 
-io.on("connection", (socket) =>{
-    console.log(`User Connected: ${socket.id}`)
-
-    socket.on("send_message", (data)=>{
-        console.log(data)
-        // socket.to(data).emit("received_message", data)
-        // socket.broadcast.emit("receive_message", data)
-        socket.emit("receive_message", data)
-    
-    })
-
-    // socket.on("send_myindex", async (data)=>{
-
-    //     console.log(data)
-
-    //     socket.emit("receive_message2", data);
-
-    //     index_bin.splice(0,1);
-    //     index_bin.push(data)
-
-    //     let sum = 0;
-
-    //     // 総和を取得
-    //     for (let i = 0; i < index_bin.length; i++) {
-    //         sum += index_bin[i];
-    //     }
-
-    //     let aveIndex = String(sum / index_bin.length)
-
-    //     console.log(aveIndex)
-
-    //     try {
-    //         socket.emit("receive_message2", aveIndex);
-    //         // socket.emit("receive_message2", {value : aveIndex});
-    //         console.log(`ave_index is ${aveIndex}`)
-    //       } catch (error) {
-    //         console.error(error);
-    //         // expected output: ReferenceError: nonExistentFunction is not defined
-    //         // (Note: the exact output may be browser-dependent)
-    //       }
-          
-
-    //     // await socket.emit("ave_index", aveIndex)
-    //     // await socket.emit("ave_index", {aveIndex})
-    //     // socket.emit("ave_index", {aveIndex})
-
-    //     // console.log(`ave_index is ${aveIndex}`)
-        
-        
-    // })
 
    
 
